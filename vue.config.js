4ca565a24031ae01852225d4202bc736ed389241
lml <soalin228@163.com>
2019-06-29 10:46:54 +0800

    项目基础配置完成

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+const resolve = dir => path.json(__dirname, dir)
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/vue' : '/' // 项目生产环境执行在域名 /vue 下，开发环境为 /
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  productionSourceMap: false, // 打包不生成 .map
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
 }
