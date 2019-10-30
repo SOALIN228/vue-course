b1acd4ca3ea3f76ae320e093d117fff7d1c32436
lml <soalin228@163.com>
2019-10-30 17:17:12 +0800

    axios 封装

--- src/api/user.js
+++ src/api/user.js
@@ -7,5 +7,10 @@ export const getUserInfo = ({ userId }) => {
     data: {
       userId
     }
+  }).then(res => { // 调用成功时操作
+    console.log(res)
+  }).catch(error => { // 调用失败时操作
+    console.log(error)
+    // 可以返回失败数据
   })
 }
