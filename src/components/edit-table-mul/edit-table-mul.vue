6683fb14a69b0051e1f204a9f9efb6da16256f23
lml <soalin228@163.com>
2019-11-18 16:04:22 +0800

    table整理

--- src/components/edit-table-mul/edit-table-mul.vue
+++ src/components/edit-table-mul/edit-table-mul.vue
@@ -18,9 +18,7 @@ export default {
   data () {
     return {
       insideData: [],
-      insideColumns: [],
-      edittingId: '',
-      edittingContent: ''
+      insideColumns: []
     }
   },
   watch: {
--- src/components/edit-table/edit-table.vue
+++ src/components/edit-table/edit-table.vue
@@ -17,50 +17,46 @@ export default {
   },
   data () {
     return {
+      insideData: [],
       insideColumns: [],
-      edittingId: '',
-      edittingContent: '',
-      inputChange: false
+      edittingId: ''
     }
   },
   watch: {
     columns () {
       this.handleColumns()
+    },
+    value () {
+      this.handleColumns()
     }
   },
   methods: {
-    handleClick ({ row, index, column }) {
+    handleClick ({ row, column, index }) {
       if (this.edittingId === `${column.key}_${index}`) {
-        if (this.inputChange) {
-          let tableData = JSON.parse(JSON.stringify(this.value))
-          tableData[index][column.key] = this.edittingContent
-          this.$emit('input', tableData)
-          this.$emit('on-edit', { row, index, column, newValue: this.edittingContent })
-        }
+        this.$emit('input', this.insideData)
+        this.$emit('on-edit', { row, column, index, newValue: this.insideData[index][column.key] })
         this.edittingId = ''
-        this.edittingContent = ''
       } else {
         this.edittingId = `${column.key}_${index}`
       }
-      this.inputChange = false
     },
-    handleInput (newValue) {
-      this.inputChange = true
-      this.edittingContent = newValue
+    handleInput (row, column, index, newValue) {
+      this.insideData[index][column.key] = newValue
     },
     handleColumns () {
+      this.insideData = JSON.parse(JSON.stringify(this.value))
       const insideColumns = this.columns.map(item => {
         if (!item.render && item.editable) {
-          item.render = (h, { row, index, column }) => {
+          item.render = (h, { row, column, index }) => {
             const isEditting = this.edittingId === `${column.key}_${index}`
             return (
               <div>
                 {
                   isEditting
-                    ? <i-input value={row[column.key]} style="width: 50px;" on-input={this.handleInput}></i-input>
+                    ? <i-input value={row[column.key]} style="width: 50px;" on-input={this.handleInput.bind(this, row, column, index)}></i-input>
                     : <span>{row[column.key]}</span>
                 }
-                <i-button on-click={this.handleClick.bind(this, { row, index, column })}>
+                <i-button on-click={this.handleClick.bind(this, { row, column, index })}>
                   {isEditting ? '保存' : '编辑'}
                 </i-button>
               </div>
