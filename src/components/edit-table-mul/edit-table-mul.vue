fed6d773a142fd387b2dbafd97b5abbdef24a7b7
lml <soalin228@163.com>
2019-11-18 15:56:51 +0800

    table整理

--- src/components/edit-table-mul/edit-table-mul.vue
+++ src/components/edit-table-mul/edit-table-mul.vue
@@ -32,36 +32,36 @@ export default {
     }
   },
   methods: {
-    handleClick ({ row, index, column }) {
+    handleClick ({ row, column, index }) {
       let keyIndex = this.insideData[index].edittingKeyArr ? this.insideData[index].edittingKeyArr.indexOf(column.key) : -1
       let rowObj = this.insideData[index]
       if (keyIndex > -1) {
         rowObj.edittingKeyArr.splice(keyIndex, 1)
         this.insideData.splice(index, 1, rowObj)
         this.$emit('input', this.insideData)
-        this.$emit('on-edit', { row, index, column, newValue: this.insideData[index][column.key] })
+        this.$emit('on-edit', { row, column, index, newValue: this.insideData[index][column.key] })
       } else {
         rowObj.edittingKeyArr = (rowObj.edittingKeyArr) ? [...rowObj.edittingKeyArr, column.key] : [column.key]
         this.insideData.splice(index, 1, rowObj)
       }
     },
-    handleInput (row, index, column, newValue) {
+    handleInput (row, column, index, newValue) {
       this.insideData[index][column.key] = newValue
     },
     handleColumns () {
       this.insideData = JSON.parse(JSON.stringify(this.value))
       const insideColumns = this.columns.map(item => {
         if (!item.render && item.editable) {
-          item.render = (h, { row, index, column }) => {
+          item.render = (h, { row, column, index }) => {
             const keyArr = this.insideData[index] ? this.insideData[index].edittingKeyArr : []
             return (
               <div>
                 {
                   keyArr && keyArr.indexOf(column.key) > -1
-                    ? <i-input value={row[column.key]} style="width: 50px;" on-input={this.handleInput.bind(this, row, index, column)}></i-input>
+                    ? <i-input value={row[column.key]} style="width: 50px;" on-input={this.handleInput.bind(this, row, column, index)}></i-input>
                     : <span>{row[column.key]}</span>
                 }
-                <i-button on-click={() => { this.handleClick({ row, index, column }) }}>
+                <i-button on-click={() => { this.handleClick({ row, column, index }) }}>
                   {keyArr && keyArr.indexOf(column.key) > -1 ? '保存' : '编辑'}
                 </i-button>
               </div>
